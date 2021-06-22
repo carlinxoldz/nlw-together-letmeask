71ea9dbd93d440c302cad4fa8b5ab1e755b1e43f
carlinxoldz <carlinxold@hotmail.com>
2021-06-22 13:37:07 -0300

    segundo commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
+
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/news" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+//import { useAuth } from '../hooks/useAuth'
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import '../styles/auth.scss';
+
+export function NewRoom() {
+    //const { user } = useAuth()
+
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="ilustração de perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <h2>Criar uma nova sala</h2>
+                    <form action="">
+                        <input 
+                            type="text"
+                            placeholder="Nome da sala"
+                        />
+                        <Button type="submit">
+                            Criar sala
+                        </Button>
+                        <p>
+                            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
+                        </p>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}

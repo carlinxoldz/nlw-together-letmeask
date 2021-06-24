baf3d0ed69ef865454c76a93b7a88fa49aee724e
carlinxoldz <carlinxold@hotmail.com>
2021-06-24 13:36:31 -0300

    quarto commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { Room } from './pages/Room';
 import { NewRoom } from "./pages/NewRoom";
+import { AdminRoom } from './pages/AdminRoom';
 import { AuthContextProvider } from './contexts/AuthContext'
+
 
 function App() {
 
@@ -11,8 +13,9 @@ function App() {
       <AuthContextProvider>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/rooms/news" exact component={NewRoom} />
+          <Route path="/rooms/news" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
